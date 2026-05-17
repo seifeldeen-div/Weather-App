2af0b8759442fac1e79601e507aa96f54450f576
Eng . Seif Eldeen <seifedeen112006@gmail.com>
2026-05-17 14:43:10 +0300

    soon

--- Js/main.js
+++ Js/main.js
@@ -283,7 +283,7 @@ window.addEventListener('beforeinstallprompt', e => {
     e.preventDefault();
     deferredPrompt = e;
     // let btn = document.querySelector('#installBtn');
-    // btn.style.display = 'block';
+    btn.style.display = 'block';
     // btn.style.animation = 'fadebtn 0.8s 0.5s ease-out forwards';
 });
 
