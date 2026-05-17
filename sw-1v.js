9f2eb04dad280179bb0e9dbd1710ab46f38935a5
Eng . Seif Eldeen <seifedeen112006@gmail.com>
2026-05-17 13:07:32 +0300

    last version

--- sw-1v.js
+++ sw-1v.js
@@ -13,7 +13,7 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
     e.respondWith(
         caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-            return caches.open('weatherpro-v1').then(cache => {
+            return caches.open('weatherpro-v2').then(cache => {
                 cache.put(e.request, res.clone());
                 return res;
             });
